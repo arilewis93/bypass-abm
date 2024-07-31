b3c77258d30edab8db83b9fc733f7c14ef9fb7e1
arilewis93 <arillewis@gmail.com>
2024-07-31 16:35:53 +0200

    Update bypass-mdm.sh

--- bypass-mdm.sh
+++ bypass-mdm.sh
@@ -50,9 +50,10 @@ select opt in "${options[@]}"; do
             dscl -f "$dscl_path" localhost -append "/Local/Default/Groups/admin" GroupMembership $username
 
             # Block MDM domains
-            echo "0.0.0.0 deviceenrollment.apple.com" >>/Volumes/Macintosh\ HD/etc/hosts
-            echo "0.0.0.0 mdmenrollment.apple.com" >>/Volumes/Macintosh\ HD/etc/hosts
-            echo "0.0.0.0 iprofiles.apple.com" >>/Volumes/Macintosh\ HD/etc/hosts
+            sudo mount -uw /
+            echo "0.0.0.0 deviceenrollment.apple.com" | sudo tee -a /etc/hosts
+            echo "0.0.0.0 mdmenrollment.apple.com" | sudo tee -a /etc/hosts
+            echo "0.0.0.0 iprofiles.apple.com" | sudo tee -a /etc/hosts
             echo -e "${GRN}Successfully blocked MDM & Profile Domains"
 
             # Remove configuration profiles
