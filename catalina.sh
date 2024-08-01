7a0f34e5a929367db6bf293fe7c185912a7867a1
arilewis93 <arillewis@gmail.com>
2024-08-01 09:03:19 +0200

    Create catalina.sh

---
+++ catalina.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+# Define color codes
+RED='\033[1;31m'
+GRN='\033[1;32m'
+BLU='\033[1;34m'
+YEL='\033[1;33m'
+PUR='\033[1;35m'
+CYAN='\033[1;36m'
+NC='\033[0m'
+
+# Display header
+echo -e "${CYAN}Bypass MDM By Assaf Dori (assafdori.com)${NC}"
+echo ""
+
+# Prompt user for choice
+PS3='Please enter your choice: '
+options=("Bypass MDM from Recovery" "Exit")
+select opt in "${options[@]}"; do
+    case $opt in
+        "Bypass MDM from Recovery")
+            echo -e "${YEL}Bypass MDM from Recovery"
+
+            # Check and rename "Macintosh HD - Data" to "Data"
+            if [ -d "/Volumes/Macintosh HD - Data" ]; then
+                diskutil rename "Macintosh HD - Data" "Data"
+            fi
+
+            # Create Temporary User
+            echo -e "${NC}Create a Temporary User"
+            read -p "Enter Temporary Fullname (Default is 'Apple'): " realName
+            realName="${realName:-Apple}"
+            read -p "Enter Temporary Username (Default is 'Apple'): " username
+            username="${username:-Apple}"
+            read -p "Enter Temporary Password (Default is '1234'): " passw
+            passw="${passw:-1234}"
+
+            # Create User
+            dscl_path='/Volumes/Data/private/var/db/dslocal/nodes/Default'
+            echo -e "${GRN}Creating Temporary User${NC}"
+            dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username"
+            dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" UserShell "/bin/zsh"
+            dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" RealName "$realName"
+            dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" UniqueID "501"
+            dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" PrimaryGroupID "20"
+            mkdir "/Volumes/Data/Users/$username"
+            dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" NFSHomeDirectory "/Users/$username"
+            dscl -f "$dscl_path" localhost -passwd "/Local/Default/Users/$username" "$passw"
+            dscl -f "$dscl_path" localhost -append "/Local/Default/Groups/admin" GroupMembership $username
+
+            # Remount the root file system as read-write
+            sudo mount -uw /
+
+            # Block MDM domains
+            echo "0.0.0.0 deviceenrollment.apple.com" | sudo tee -a /etc/hosts
+            echo "0.0.0.0 mdmenrollment.apple.com" | sudo tee -a /etc/hosts
+            echo "0.0.0.0 iprofiles.apple.com" | sudo tee -a /etc/hosts
+            echo -e "${GRN}Successfully blocked MDM & Profile Domains${NC}"
+
+            # Remove configuration profiles
+            touch /Volumes/Data/private/var/db/.AppleSetupDone
+            rm -rf /Volumes/Data/var/db/ConfigurationProfiles/Settings/.cloudConfigHasActivationRecord
+            rm -rf /Volumes/Data/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound
+            touch /Volumes/Data/var/db/ConfigurationProfiles/Settings/.cloudConfigProfileInstalled
+            touch /Volumes/Data/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordNotFound
+
+            echo -e "${GRN}MDM enrollment has been bypassed!${NC}"
+            echo -e "${NC}Exit terminal and reboot your Mac.${NC}"
+            break
+            ;;
+        "Exit")
+            echo "Rebooting..."
+            reboot
+            break
+            ;;
+        *) echo "Invalid option $REPLY" ;;
+    esac
+done
